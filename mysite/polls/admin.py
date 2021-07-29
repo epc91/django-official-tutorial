02d61dbc07c3e727ce1200e288b4dca7cf89464d
Eduardo Peschke <eduardopeschke@gmail.com>
2021-07-29 15:56:57 -0400

    Admin Site Custom

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,30 @@
 from django.contrib import admin
 
-from . models import Question
+from . models import Question, Choice
 
 # Register your models here.
 
-admin.site.register(Question)
+# Associated models
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 2
+
+class QuestionAdmin(admin.ModelAdmin):
+    # Add field groups in Admin
+    fieldsets = [
+        ('Question', {
+            'fields': ['question_text']
+        }),
+        ('Date Information', {
+            'fields': ['pub_date']
+        })
+    ]
+    inlines = [ChoiceInLine]
+
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+
+    list_filter = ['pub_date']
+
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
